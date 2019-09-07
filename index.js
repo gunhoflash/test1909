d622805d7a2aaaf948cb0d26a4a31d859e0a9d86
Geonho Kim <gunhoflash@gmail.com>
2019-09-07 20:54:09 +0900

    Self-Request Test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 
 var server = require('http').createServer(app);
@@ -18,4 +19,40 @@ console.log(`listen now with port:${port}`);
 let i = 0;
 let intervalObj = setInterval(() => {
 	console.log(`interviewing the interval ${i++}`);
-}, 60000);
+	sendPOST();
+}, 6000);
+
+sendPOST = () => {
+
+	const data = JSON.stringify({
+		hello: 'world'
+	});
+
+	const options = {
+		hostname: 'http://test1909.herokuapp.com',
+		port: 443,
+		path: '/selfRequest',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': data.length
+		}
+	};
+
+	const req = https.request(options, (res) => {
+		console.log(`statusCode: ${res.statusCode}`)
+		res.on('data', process.stdout.write);
+	});
+
+	req.on('error', console.error);
+	req.write(data);
+	req.end();
+};
+
+app.post('/selfRequest', (req, res) => {
+	console.log('/selfRequest');
+	console.log(req.body);
+	res.json({
+		hello: 'world'
+	});
+});
